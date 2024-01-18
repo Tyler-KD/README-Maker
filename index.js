75d1c31518f77b44fccbd07557d6caf54ca1871b
unknown <tyler.kd.knapp@gmail.com>
2024-01-18 16:38:16 -0600

    Added questions for user input

--- index.js
+++ index.js
@@ -5,15 +5,58 @@ const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title",        
+    },
+    {
+        type: "input",
+        message: "Provide a short description explaining the what, why, and how of your project:",
+        name: "description",        
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "installation",        
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use:",
+        name: "usage",        
+    },
+    {
+        type: "checkbox",
+        message: "Which license is your application covered under?",
+        name: "license",        
+    },
+    {
+        type: "input",
+        message: "List your collaborators, if any, with links to their GitHub profiles:",
+        name: "contributing",        
+    },
+    {
+        type: "input",
+        message: "Write tests for your application and provide examples on how to run them here:",
+        name: "tests",        
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "author",        
+    },
+    {
+        type: "input",
+        message: "Enter your email address:",
+        name: "email",        
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 function init() {
     inquirer.prompt(questions).then((responses) => {
         console.log("Making Professional README.md File...");
@@ -21,4 +64,5 @@ function init() {
     });
 }
 
+// Function call to initialize app
 init();
